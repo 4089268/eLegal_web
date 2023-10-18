99e45bea1c2d0c87e4f335a5fc1a19b29c119919
Salvador M <chava_r17@hotmail.com>
2023-10-18 00:02:52 -0600

    Feat: Creacion de formulario para registro de entrada

---
+++ Models/NewEntryViewModel.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace eLegal_web.Models {
+    public class NewEntryViewModel  {
+        
+        public string Folio {get;set;} = "";
+        public int IdUsuarioRegistro {get;set;} = 0;
+        public string UsuarioRegistro {get;set;} = "";
+        public DateTime FechaRegistro {get;set;} = DateTime.Now;
+
+
+        [Required(ErrorMessage = "El Origen es requerido")]
+        public int IdOrigen {get;set;} = 0;
+
+        [Required(ErrorMessage = "El campo Asunto es requerido")]
+        public string Asunto {get;set;}  = "";
+
+        [Required(ErrorMessage = "El campo Oficina de Origen es requerido")]
+        public string OficinaOrigen {get;set;}  = "";
+        
+        [Required(ErrorMessage = "El campo Fecha Documento es requerido")]
+        public DateTime FechaDocumento {get;set;} = DateTime.Now;
+        
+        [Required(ErrorMessage = "El campo Numero Documento es requerido")]
+        public string NumeroDocumento {get;set;}  = "";
+        
+        [Required(ErrorMessage = "El campo Tipo Entrada es requerido")]
+        public int IdTipoEntrada {get;set;} = 0;
+        
+        [Required(ErrorMessage = "El campo Descripcion es requerido")]
+        public string Descripcion {get;set;} = "";
+        
+        [Required(ErrorMessage = "Se debe seleccionar minimo un Departamento")]
+        [MinLength(1, ErrorMessage = "Se debe seleccionar minimo un Departamento")]
+        public List<int> Departamentos {get;set;} = new List<int>();
+
+    }
+}
